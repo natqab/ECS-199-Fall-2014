a1b2e4722232103647f6c89130eff20f013164d2
Corina Putinar <cputinar@gmail.com>
2014-10-29 23:08:00 -0700

    Created Sign In Page with Button

---
+++ ECS199/ECS199/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECS199
+//
+//  Created by Corina Putinar on 10/29/14.
+//  Copyright (c) 2014 ecs199. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
